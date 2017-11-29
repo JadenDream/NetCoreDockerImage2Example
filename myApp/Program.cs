28ed2379387945df816a3e8ade2024013f520950
brady <brady@xsg.com.tw>
2017-11-29 14:17:49 +0800

    ++ myApp, myLibrary

---
+++ myApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			myLibrary.Class1.TestRun();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ myLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myLibrary
+{
+    public class Class1
+    {
+		static public void TestRun()
+		{
+			Console.WriteLine("Run {0}.{1}.", System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName
+                , System.Reflection.MethodBase.GetCurrentMethod().Name);
+        }
+    }
+}
