9ceac03bb1ac40f4807f7cf3ef66a914a5e0fb01
brady <brady@xsg.com.tw>
2017-11-29 16:35:16 +0800

    排版調整

--- myLibrary/Class1.cs
+++ myLibrary/Class1.cs
@@ -6,8 +6,9 @@ namespace myLibrary
     {
 		static public void TestRun()
 		{
-			Console.WriteLine("Run {0}.{1}.", System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName
-                , System.Reflection.MethodBase.GetCurrentMethod().Name);
+			Console.WriteLine("Run {0}.{1}."
+				, System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName
+				, System.Reflection.MethodBase.GetCurrentMethod().Name);
         }
     }
 }
